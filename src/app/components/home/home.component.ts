22d0573087b34a55689146021d4ce8d516f8567d
vikrant02-dev <amritraj.work12@gmail.com>
2020-10-10 11:55:38 +0530

    piechart

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, NgZone, PLATFORM_ID, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+// amCharts imports
+import * as am4core from '@amcharts/amcharts4/core';
+import * as am4charts from '@amcharts/amcharts4/charts';
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +12,117 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  private chart: am4charts.XYChart;
+  private pieChart: am4charts.PieChart;
+    
+  constructor(@Inject(PLATFORM_ID) private platformId, private zone: NgZone) { }
 
   ngOnInit() {
   }
+
+    // Run the function only in the browser
+    browserOnly(f: () => void) {
+      if (isPlatformBrowser(this.platformId)) {
+        this.zone.runOutsideAngular(() => {
+          f();
+        });
+      }
+    }
+
+    ngAfterViewInit() {
+      // Chart code goes in here
+      this.makeSampleChart();
+      this.makePieChart();
+    }
+  
+
+    makePieChart() {
+      this.browserOnly(() => {
+        am4core.useTheme(am4themes_animated);
+  
+        let chart = am4core.create("pieChartdiv", am4charts.PieChart);
+        chart.data = [{
+          "country": "Lithuania",
+          "litres": 501.9
+        }, {
+          "country": "Czech Republic",
+          "litres": 301.9
+        }, {
+          "country": "Ireland",
+          "litres": 201.1
+        }, {
+          "country": "Germany",
+          "litres": 165.8
+        }, {
+          "country": "Australia",
+          "litres": 139.9
+        }, {
+          "country": "Austria",
+          "litres": 128.3
+        }, {
+          "country": "UK",
+          "litres": 99
+        }, {
+          "country": "Belgium",
+          "litres": 60
+        }, {
+          "country": "The Netherlands",
+          "litres": 50
+        }];
+        let pieSeries = chart.series.push(new am4charts.PieSeries());
+        pieSeries.dataFields.value = "litres";
+        pieSeries.dataFields.category = "country";
+        
+        this.pieChart = chart;
+      });
+    }
+
+    makeSampleChart() {
+      this.browserOnly(() => {
+        am4core.useTheme(am4themes_animated);
+  
+        let chart = am4core.create("chartdiv", am4charts.XYChart);
+  
+        chart.paddingRight = 20;
+  
+        let data = [];
+        let visits = 10;
+        for (let i = 1; i < 366; i++) {
+          visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+          data.push({ date: new Date(2018, 0, i), name: "name" + i, value: visits });
+        }
+  
+        chart.data = data;
+  
+        let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+        dateAxis.renderer.grid.template.location = 0;
+  
+        let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        valueAxis.tooltip.disabled = true;
+        valueAxis.renderer.minWidth = 35;
+  
+        let series = chart.series.push(new am4charts.LineSeries());
+        series.dataFields.dateX = "date";
+        series.dataFields.valueY = "value";
+        series.tooltipText = "{valueY.value}";
+  
+        chart.cursor = new am4charts.XYCursor();
+  
+        let scrollbarX = new am4charts.XYChartScrollbar();
+        scrollbarX.series.push(series);
+        chart.scrollbarX = scrollbarX;
+  
+        this.chart = chart;
+      });
+    }
+
+    ngOnDestroy() {
+      // Clean up chart when the component is removed
+      this.browserOnly(() => {
+        if (this.chart) {
+          this.chart.dispose();
+        }
+      });
+    }  
 
 }
